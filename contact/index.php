35da75be5b57233f6507b05523c94ec7ea3567d7
Alex Brown <alex@cohoda.com>
2016-11-26 11:10:28 +0000

    email fixes

--- contact/index.php
+++ contact/index.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'alex@cohoda.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Cohoda Website Contact Form:  $name";
-$email_body = "You have received a new message from the contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "Name:\n\ $name\n\nEmail:\n\ $email_address\n\nPhone:\n\ $phone\n\nMessage:\n$message";
 $headers = "From: ". $name ." <noreply@cohoda.com>\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
