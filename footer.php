c7af00f32a4bc1820a5a7b15dd0685484f65eb17
Alex Brown <alex@cohoda.com>
2024-07-02 23:15:50 +0100

    update footer

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-				<span class="copyright">Copyright &copy; Cohoda LTD - 2016</span>
+				<span class="copyright">Copyright &copy; Cohoda LTD - 2024</span>
 			</div>
 			<div class="col-md-4">
 				<ul class="list-inline social-buttons">
+					<li><a href="https://www.instagram.com/cohoda"><i class="fa fa-instagram"></i></a>
+					</li>
 					<li><a href="https://www.twitter.com/cohoda"><i class="fa fa-twitter"></i></a>
 					</li>
 					<li><a href="https://www.facebook.com/cohoda"><i class="fa fa-facebook"></i></a>
